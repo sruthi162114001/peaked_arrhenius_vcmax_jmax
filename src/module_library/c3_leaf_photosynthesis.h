4b138b02f08a3e957f2c1dd3f9328d3d0e60a3b1
HaripriyaSMinnu <144206263+HaripriyaSMinnu@users.noreply.github.com>
2026-02-10 19:36:22 +0530

    Replace Ea with Ha and add Hd, S for Jmax and Vcmax
    Renamed Jmax_Ea → Jmax_Ha
    Renamed Vcmax_Ea → Vcmax_Ha
    Added Jmax_Hd, Jmax_S
    Added Vcmax_Hd, Vcmax_S  in accordance with  Johnson, Eyring & Williams temperature response formulation

--- src/module_library/c3_leaf_photosynthesis.h
+++ src/module_library/c3_leaf_photosynthesis.h
@@ -39,7 +39,9 @@ class c3_leaf_photosynthesis : public direct_module
           height{get_input(input_quantities, "height")},
           Jmax_at_25{get_input(input_quantities, "Jmax_at_25")},
           Jmax_c{get_input(input_quantities, "Jmax_c")},
-          Jmax_Ea{get_input(input_quantities, "Jmax_Ea")},
+          Jmax_Ha{get_input(input_quantities, "Jmax_Ha")},
+          Jmax_Hd{get_input(input_quantities, "Jmax_Hd")},
+          Jmax_S{get_input(input_quantities, "Jmax_S")},
           Kc_c{get_input(input_quantities, "Kc_c")},
           Kc_Ea{get_input(input_quantities, "Kc_Ea")},
           Ko_c{get_input(input_quantities, "Ko_c")},
@@ -64,7 +66,9 @@ class c3_leaf_photosynthesis : public direct_module
           Tp_S{get_input(input_quantities, "Tp_S")},
           Vcmax_at_25{get_input(input_quantities, "Vcmax_at_25")},
           Vcmax_c{get_input(input_quantities, "Vcmax_c")},
-          Vcmax_Ea{get_input(input_quantities, "Vcmax_Ea")},
+          Vcmax_Ha{get_input(input_quantities, "Vcmax_Ha")},
+          Vcmax_Hd{get_input(input_quantities, "Vcmax_Hd")},
+          Vcmax_S{get_input(input_quantities, "Vcmax_S")},
           windspeed{get_input(input_quantities, "windspeed")},
 
           // Get pointers to output quantities
@@ -108,7 +112,9 @@ class c3_leaf_photosynthesis : public direct_module
     double const& height;
     double const& Jmax_at_25;
     double const& Jmax_c;
-    double const& Jmax_Ea;
+    double const& Jmax_Ha;
+    double const& Jmax_Hd;
+    double const& Jmax_S;
     double const& Kc_c;
     double const& Kc_Ea;
     double const& Ko_c;
@@ -133,7 +139,9 @@ class c3_leaf_photosynthesis : public direct_module
     double const& Tp_S;
     double const& Vcmax_at_25;
     double const& Vcmax_c;
-    double const& Vcmax_Ea;
+    double const& Vcmax_Ha;
+    double const& Vcmax_Hd;
+    double const& Vcmax_S;
     double const& windspeed;
 
     // Pointers to output quantities
