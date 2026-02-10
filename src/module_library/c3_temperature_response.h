fc6f5a1e03bd74d1cabe691f090570726440f0af
HaripriyaSMinnu <144206263+HaripriyaSMinnu@users.noreply.github.com>
2026-02-10 18:40:23 +0530

    Update comments for Jmax and Vcmax parameters

--- src/module_library/c3_temperature_response.h
+++ src/module_library/c3_temperature_response.h
@@ -4,7 +4,7 @@
 struct c3_temperature_response_parameters {
     double Gstar_c;     //!< Arrhenius scaling parameter for Gstar (dimensionless)
     double Gstar_Ea;    //!< Arrhenius activation energy for Gstar (J / mol)
-    double Jmax_c;      //!< Arrhenius scaling parameter for Jmax_norm
+    double Jmax_c;      //!< Johnson, Eyring, & Williams scaling parameter for Jmax_norm
     double Jmax_Ha;     //!< Johnson, Eyring, & Williams enthalpy of activation for Jmax_norm (J / mol)
     double Jmax_Hd;     //!< Johnson, Eyring, & Williams enthalpy of deactivation for Jmax_norm (J / mol)
     double Jmax_S;      //!< Johnson, Eyring, & Williams entropy parameter for Jmax_norm (J / K / mol)
@@ -24,7 +24,7 @@ struct c3_temperature_response_parameters {
     double Tp_Ha;       //!< Johnson, Eyring, & Williams enthalpy of activation for Tp_norm (J / mol)
     double Tp_Hd;       //!< Johnson, Eyring, & Williams enthalpy of deactivation for Tp_norm (J / mol)
     double Tp_S;        //!< Johnson, Eyring, & Williams entropy parameter for Tp_norm (J / K / mol)
-    double Vcmax_c;     //!< Arrhenius scaling parameter for Vcmax_norm (dimensionless)
+    double Vcmax_c;     //!< Johnson, Eyring, & Williams scaling parameter for Vcmax_norm (dimensionless)
     double Vcmax_Ha;    //!< Johnson, Eyring, & Williams activation energy for Vcmax_norm (J / mol)
     double Vcmax_Hd;    //!< Johnson, Eyring, & Williams enthalpy of deactivation for Vcmax_norm (J / mol)
     double Vcmax_S;     //!< Johnson, Eyring, & Williams entropy parameter for Vcmax_norm (J / K / mol)
